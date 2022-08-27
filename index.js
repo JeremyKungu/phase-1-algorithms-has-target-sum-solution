3e73b131700095e01043240fac9f5e7e16d54985
JeremyKungu <jeremikungu27@gmail.com>
2022-08-27 14:56:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for(let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  } 
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
 /* 
   Add your pseudocode here
 */
